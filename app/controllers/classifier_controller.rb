548cb1c91e897406a18ea714a37c4410fe00937a
Jeffrey Warren <jeff@unterbahn.com>
2011-10-30 12:42:54 -0400

    starting to take shape in classifier_controller.rb

--- app/controllers/classifier_controller.rb
+++ app/controllers/classifier_controller.rb
@@ -7,25 +7,24 @@ class ClassifierController < ApplicationController
 		a.save
 	end
 
-	# this code will not yet run
 	def train
 		# Soon we'll start accepting an image, but for now just a pixel with n color values
 
-		#pixels = []
-		#@image = Image.new
-		#@image.url = params[:url]
-		#@image.save
+			#pixels = []
+			#@image = Image.new
+			#@image.url = params[:url]
+			#@image.save
 
-		# some stuff here
+			# some stuff here
 
-		#img = Magick::ImageList.new(filename)
-		#img.each_pixel do |pixel, c, r|
-		#	pixels.push(pixel)
-		#end
-		# pixels now contains each individual pixel of img, we can pass it to a Classifier model?
+			#img = Magick::ImageList.new(filename)
+			#img.each_pixel do |pixel, c, r|
+			#	pixels.push(pixel)
+			#end
+			# pixels now contains each individual pixel of img, we can pass it to a Classifier model?
 
-		#classifier = Classifier.find(params[:something])
-		#classifier.train(pixels,params[:classname],params[:model]) # model = the name of the model we're training
+			#classifier = Classifier.find(params[:something])
+			#classifier.train(pixels,params[:classname],params[:model]) # model = the name of the model we're training
 
 		# the following belongs in the Classifier model, unless Tom rejects abstraction...
 		# for now, let's stick with a model-less system where you pass a :model parameter with every request
@@ -34,6 +33,9 @@ class ClassifierController < ApplicationController
 		if File.find('classifiers/'+params[:model])
 			a = NaiveBayes.load('classifiers/'+params[:model])
 
+			# encode RGB or NRGB color data as an n-dimensional vector, transform to string?
+		
+			# this is probably not a good idea:	
 			params[:bands].each do |band|
 				#a.train(params[:class], 'bad', 'word') # multiple terms
 				a.train(params[:class], band, 'red') # how do we name the feature type, r,g, or b?
@@ -49,6 +51,8 @@ class ClassifierController < ApplicationController
 
 	def classify
 		# b = "this is a bad sentence".split(' ')
+
+		# encode RGB or NRGB color data as an n-dimensional vector, transform to string?
 		b = [params[:r],params[:g],params[:b]] # how to search on combined color value, 
 							# or specify feature type from r,g,b?
 		a = NaiveBayes.load('classifiers/'+params[:model]+'.nb') # select a trained model
